ca877d5ea1b88c1bd7dc07140b1ddf8742996c08
Enes <mgid9133@uni.sydney.edu.au>
2023-11-28 13:34:56 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+function App() {  
+  return (    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Job salary={90000} position="Senior SDE" company="Amazon"/>
+      <Job salary={12000} position="Junior SDE" company="Google"/>
+      <Job salary={10000} position="Project Manager" company="Netflix"/>
+    </div>
+  );
+}
+
+function Job(props) {
+  return (
+    <div>
+      <h1>Company = {props.company}</h1>
+      <h2>Position = {props.position}</h2>
+      <h3>Salary = {props.salary}</h3>
     </div>
   );
 }
